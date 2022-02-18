81935506572d8501c8c53d830a48d5167e0795c3
josh <61042416+joshuafirme@users.noreply.github.com>
2022-02-18 17:57:16 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,10 +17,10 @@
 
   <!-- Vendor CSS Files -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-  <link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{asset('assets/css/coming_soon.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/css/coming_soon.css')}}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: ComingSoon - v4.6.0
