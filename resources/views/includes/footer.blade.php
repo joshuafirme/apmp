8624469ffba13d25dd7eee7f57ff72b367bd3d76
josh <61042416+joshuafirme@users.noreply.github.com>
2022-03-11 18:42:00 +0800

    Update footer.blade.php

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -63,6 +63,8 @@
   <script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
   <script src="{{asset('assets/vendor/purecounter/purecounter.js')}}"></script>
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
   <!-- Template Main JS File -->
   <script src="{{asset('assets/js/landing.js')}}"></script>
 
